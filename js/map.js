a004869b3e97d7dab17aa4694123e85faa5a7c82
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2014-12-22 18:16:41 -0700

    Fixed formatting, removed extra spacing

--- js/map.js
+++ js/map.js
@@ -7,49 +7,49 @@
   var eventsLayer = L.mapbox.featureLayer().addTo(map);
 
   // Kickoff on document ready event
-		  $(document).ready(function() {
-        
-        fetchJSONFile('workshops.json', function(data){
-				  eventsLayer.setGeoJSON(data);
-			  });
+  $(document).ready(function() {
 
-        // Map "eventsLayer" layer UI events
-        eventsLayer.on('mouseover', function(e) {
-          e.layer.openPopup();
-        });
-        eventsLayer.on('mouseout', function(e) {
-          setTimeout( function() {
-            e.layer.closePopup()
-          }, 4000);
-        });
+    fetchJSONFile('workshops.json', function(data){
+      eventsLayer.setGeoJSON(data);
+    });
+
+    // Map "eventsLayer" layer UI events
+    eventsLayer.on('mouseover', function(e) {
+      e.layer.openPopup();
+    });
+    eventsLayer.on('mouseout', function(e) {
+      setTimeout( function() {
+        e.layer.closePopup()
+      }, 4000);
+    });
+
+    // Navigation bar events
+    $("#header-hotspot").mouseenter(function() {
+      $("#header-container").animate({
+        top: 0
+      }, 500);
+    });
+		$("#header-hotspot").mouseleave(function() {
+      $("#header-container").animate({
+        top: -150
+      }, 1000);
+    });
         
-        // Navigation bar events
-        $("#header-hotspot").mouseenter(function() {
-          $("#header-container").animate({ 
-            top: 0 
-          }, 500);  
-        });
-				$("#header-hotspot").mouseleave(function() {
-          $("#header-container").animate({ 
-            top: -150 
-          }, 1000);  
-        });
-        
-      });
-      
-      /**
-       * Gather map data from JSON file
-       */
-      function fetchJSONFile(path, callback) {
-				var httpRequest = new XMLHttpRequest();
-				httpRequest.onreadystatechange = function() {
-					if (httpRequest.readyState === 4) {
-						if (httpRequest.status === 200) {
-							var data = JSON.parse(httpRequest.responseText);
-							if (callback) callback(data);
-						}
-					}
-				};
-				httpRequest.open('GET', path);
-				httpRequest.send();
-			}
+  });
+
+  /**
+   * Gather map data from JSON file
+   */
+  function fetchJSONFile(path, callback) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+      if (httpRequest.readyState === 4) {
+        if (httpRequest.status === 200) {
+          var data = JSON.parse(httpRequest.responseText);
+          if (callback) callback(data);
+        }
+      }
+    };
+    httpRequest.open('GET', path);
+    httpRequest.send();
+  }
